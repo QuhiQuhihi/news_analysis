adb9853c3dcdc05da9bf314b39ba2edff781dcf3
Ubuntu <ubuntu@ip-172-31-40-54.ap-northeast-2.compute.internal>
2022-03-25 13:37:28 +0000

    aws rds insert test completed

--- daily_job.py
+++ daily_job.py
@@ -7,53 +7,33 @@ import pandas as pd
 from datetime import datetime
 
 from news_collect import news_crawl
-from news_data_insert import insert_news_data
+from news_data_insert import db_conn_test
 
 
-class daily_job():
-    def __init__(self, today=None) -> None:
+class daily_job:
+    def __init__(self, today=None):
         
         if today == None:
-            today = datetime.datetime.today()
-            today = today.strftime('%Y-%m-%d')
+            today = datetime.today()
+            self.today = today.strftime('%Y-%m-%d')
         
         self.base_dir = os.getcwd()
         self.data_dir = os.path.join(self.base_dir,'data')
         self.news_dir = os.path.join(self.data_dir,'news')
     
-    def main_job(self, today):
+    def main_job(self):
         
-        # print("--- news crawling started ---")
-        # news_data = news_crawl.crawl_news_data()
-        # print("--- news crawling finished ---")
-        # print("--- insert to news_analysis started ---")
-        # insert_news_data.db_conn_test(news_data)
-        # print("--- insert to news_analysis finished ---")
-
-        print("2022-03-21")
-        news_data = pd.read_excel(os.path.join(self.data_dir,'2022-03-21_news.xlsx'))
+        print("--- news crawling started ---")
+        news_data = news_crawl.crawl_news_data()
+        print("--- news crawling finished ---")
+        print("--- insert to news_analysis started ---")
         insert_news_data.db_conn_test(news_data)
-
-        print("2022-03-22")
-        news_data = pd.read_excel(os.path.join(self.data_dir,'2022-03-22_news.xlsx'))
-        insert_news_data.db_conn_test(news_data)
-
-        print("2022-03-23")
-        news_data = pd.read_excel(os.path.join(self.data_dir,'2022-03-23_news.xlsx'))
-        insert_news_data.db_conn_test(news_data)
-
-        print("2022-03-24")
-        news_data = pd.read_excel(os.path.join(self.data_dir,'2022-03-24_news.xlsx'))
-        insert_news_data.db_conn_test(news_data)
-    
-        print("2022-03-25")
-        news_data = pd.read_excel(os.path.join(self.data_dir,'2022-03-25_news.xlsx'))
-        insert_news_data.db_conn_test(news_data)
-
+        print("--- insert to news_analysis finished ---")
 
         print("--- insert to news_analysis finished ---")
 
 if __name__=='__main__':
-    daily_job.main_job()
+    day_job = daily_job('2022-03-25')
+    day_job.main_job()
 
 
--- news_data_insert.py
+++ news_data_insert.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import datetime
+import traceback
 import numpy as np
 import pandas as pd
 
@@ -10,54 +12,52 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker
 
-class insert_news_data():
-    def __init__(self) -> None:
-        pass
-
-    def create_db_engine(config: Configuration) -> Engine:
-        url = 'postgresql://{user}:{password}@{host}:{port}/{db}'
-        url = url.format(
-            user=config.user,
-            password=config.password,
-            host=config.host,
-            port=config.port,
-            db=config.dbname,
-        )
-        return create_engine(url, echo=config.echo)
+def create_db_engine(config: aws_rds_config) -> Engine:
+    url = 'postgresql://{user}:{password}@{host}:{port}/{db}'
+    url = url.format(
+        user=config.user,
+        password=config.password,
+        host=config.host,
+        port=config.port,
+        db=config.dbname,
+    )
+    return create_engine(url)
 
 
-    def create_db_session(engine: Engine):
-        session = sessionmaker(bind=engine, autocommit=False)
-        return session
+def create_db_session(engine: Engine):
+    session = sessionmaker(bind=engine, autocommit=False)
+    return session
 
-    def db_conn_test(df_insert):
+def db_conn_test(df_insert):
 
-        config = aws_rds_config()
-        db_engine = create_db_engine(config=config)  # DB 엔진 정의
-        db_engine.connect()  # DB 커넥션 생성
-        session = create_db_session(engine=db_engine)  # DB 세션 생성
-        df_insert.loc[:,'created_at'] = datetime.datetime.now()
-        df_insert.loc[:,'updated_at'] = datetime.datetime.now()
+    config = aws_rds_config()
+    db_engine = create_db_engine(config=config)  # DB 엔진 정의
+    db_engine.connect()  # DB 커넥션 생성
+    session = create_db_session(engine=db_engine)  # DB 세션 생성
+    df_insert.loc[:,'created_at'] = datetime.datetime.now()
+    df_insert.loc[:,'updated_at'] = datetime.datetime.now()
 
 
-        try:
-            session = session()  # SQLAlchemy 에서 사용할 세션 정의
-            df_insert.to_sql('news_data', con=db_engine, schema='public', if_exists='append', index=False,
-                    dtype={
-                        'date': sqlalchemy.VARCHAR(100),
-                        'publish_date': sqlalchemy.DATETIME(),
-                        'news_source': sqlalchemy.VARCHAR(100),
-                        'news_topic': sqlalchemy.VARCHAR(100),
-                        'news_title': sqlalchemy.VARCHAR(100),
-                        'news_keyword': sqlalchemy.VARCHAR(100),
-                        'news_link': sqlalchemy.VARCHAR(100),
-                        'news_text' : sqlalchemy.TEXT(),
-                        'created_at':sqlalchemy.DATETIME(),
-                        'updated_at': sqlalchemy.DATETIME()
-                    })
-        except Exception as error:
-            print("db error")
-            # raise error
+    try:
+        session = session()  # SQLAlchemy 에서 사용할 세션 정의
+        df_insert.to_sql('news_data', con=db_engine, schema='public', if_exists='append', index=False,
+                dtype={
+                    'date': sqlalchemy.VARCHAR(100),
+                    'publish_date': sqlalchemy.DATETIME(),
+                    'news_source': sqlalchemy.VARCHAR(100),
+                    'news_topic': sqlalchemy.VARCHAR(100),
+                    'news_title': sqlalchemy.VARCHAR(100),
+                    'news_keyword': sqlalchemy.VARCHAR(100),
+                    'news_link': sqlalchemy.VARCHAR(100),
+                    'news_text' : sqlalchemy.TEXT(),
+                    'created_at':sqlalchemy.DATETIME(),
+                    'updated_at': sqlalchemy.DATETIME()
+                })
+    except Exception as error:
+        print("db error")
+        # raise error
+        msg = '{}'.format(traceback.format_exc())
+        print(msg)
 
-        finally:
-            session.close()  # DB 세션 닫기
+    finally:
+        session.close()  # DB 세션 닫기
